871ccee56ac6fc1aa8d70c021b317f5a8fa2d6a8
ManuelRoperoM <mnlrpr@gmail.com>
2024-06-28 15:19:53 -0500

    Encriptacion

--- encryptor.js
+++ encryptor.js
@@ -5,18 +5,55 @@ const textChange = () => {
     const regex = /^[a-z\s]*$/
     if (!regex.test(textArea)) {
         encryptButton.disabled = true
-        unencrypt.disabled = true
+        unencryptButton.disabled = true
         encryptButton.classList.add('disabled-button')
-        unencrypt.classList.add('disabled-button')
+        unencryptButton.classList.add('disabled-button')
     }else{
         encryptButton.disabled = false
-        unencrypt.disabled = false
+        unencryptButton.disabled = false
         encryptButton.classList.remove('disabled-button')
-        unencrypt.classList.remove('disabled-button')
+        unencryptButton.classList.remove('disabled-button')
     }
+}
+
+function disabled(){
+    document.getElementById('doll-img').classList.add('hidden')
+    document.getElementById('pictuare-subtitle').classList.add('hidden')
+    document.getElementById('pictuare-parrafo').classList.add('hidden')
+    document.getElementById('encrypted-message').classList.remove('hidden')
+}
+function enabled(){
+    document.getElementById('doll-img').classList.remove('hidden')
+    document.getElementById('pictuare-subtitle').classList.remove('hidden')
+    document.getElementById('pictuare-parrafo').classList.remove('hidden')
+    document.getElementById('encrypted-message').classList.add('hidden')
 }
 
 const encryptor = () => {
     const textArea = document.getElementById('text-user').value
-    console.log('clickEncriptar',textArea);
+    let encryptText = '';
+    if (textArea) {
+        const encryptDcitionary = {
+            'a':'ai',
+            'e': 'enter',
+            'i': 'imes',
+            'o': 'ober',
+            'u': 'ufat' 
+        }
+        for(let i = 0; i < textArea.length; i++){
+            if (encryptDcitionary.hasOwnProperty(textArea[i])) {
+                encryptText += encryptDcitionary[textArea[i]]
+            }else{
+                encryptText +=textArea[i]
+            }
+        }
+        disabled() //desahbilitar elementos
+        const encryptedMssge = document.getElementById('encrypted-message')
+        encryptedMssge.style.display = 'flex'
+        encryptedMssge.innerHTML = encryptText
+        console.log(encryptText)
+    }else{
+        console.log('texto vacio');
+        enabled()
+    }
 }
